7297ec5d9eea7fa33e73bc7bf3e5d8a48428527a
Dmitry Tebaykin <tebaykin@PATH-C02TV0SDHTDH.local>
2021-11-05 10:21:41 -0700

    Actually added compositeTIF plugin, previous commit missed it

---
+++ plugins/compositeTIF.plg/compositeTIF.py
@@ -0,0 +1,47 @@
+# start custom imports - DO NOT MANUALLY EDIT BELOW
+# end custom imports - DO NOT MANUALLY EDIT ABOVE
+from PyQt5 import QtWidgets, QtCore, QtGui, uic
+
+from amp.main_viewer import MainViewer
+from amp.mplwidget import ImagePlot, HistPlot, Plot
+
+import numpy as np
+
+import os
+import shutil
+import json
+import traceback
+
+from typing import Tuple, List, Dict, Any, Union
+
+class CompositeTIF(QtWidgets.QMainWindow):
+
+    def __init__(self, main_viewer: MainViewer, ui_path: str):
+        # start typedef - DO NOT MANUALLY EDIT BELOW
+        # end typedef - DO NOT MANUALLY EDIT ABOVE
+
+        super().__init__()
+
+        # set reference to main window
+        self.main_viewer = main_viewer
+
+        # load ui elements
+        uic.loadUi(
+            ui_path,
+            self
+        )
+
+        self.setWindowTitle("Composite TIF creation")
+
+# function for amp plugin building
+def build_as_plugin(main_viewer: MainViewer, plugin_path: str) -> CompositeTIF:
+    """ Returns an instance of CompositeTIF
+
+    This function is common to all plugins; it allows the plugin loader
+    to correctly load the plugin.
+
+    Args:
+        main_viewer (MainViewer): reference to the main window
+    """
+
+    return CompositeTIF(main_viewer, plugin_path)
